4b83468c21290f3bab8cd2dc661c2c0d69e2bc5d
rikeurtnputri <rikeuratnaputri@gmail.com>
2026-09-11 13:22:24 +0700

    first commit

--- src/components/Ucapan.jsx
+++ src/components/Ucapan.jsx
@@ -20,7 +20,7 @@ function Ucapan() {
     pesan: "",
   });
 
-  const API_URL = "sb_publishable_wO-BR3PPhrLNU7ZkIYpMdw_27AKVhro";
+  const API_URL = "https://tknmxqrrxrvdqtpomjni.supabase.co";
 
   /* =========================
      AMBIL DATA UCAPAN
